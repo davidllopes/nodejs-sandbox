f479e4e29921a8af0ca5f6101b4f7fc10692c7fa
David <davidllopes@gmail.com>
2023-01-19 12:14:33 +0000

    first commit

---
+++ hello.js
@@ -0,0 +1,10 @@
+const lodash = require("lodash");
+
+let items = ["spoon", "fork", "knife", "vase", "picture"];
+let temp = lodash.first(items);
+
+console.log(temp);
+
+let test = lodash.random(0, 55);
+
+console.log(test);
---
+++ hi.js
@@ -0,0 +1,3 @@
+import chalk from "chalk";
+
+console.log(`${chalk.red("Hi there!")} How are you today?`);
